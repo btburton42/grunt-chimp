f1fa1b0935440126f7c51b2512b671725b2c08c3
Tyler Burton <btburton42@gmail.com>
2016-07-19 16:30:54 -0700

    refactor test to be same as BDD

--- example/specs/example.js
+++ example/specs/example.js
@@ -1,8 +1,10 @@
-describe('Chimp Mocha', () => {
-  describe('Page title', () => {
-    it('should reflect the current page title', () => {
-      browser.url('http://www.google.com');
-      expect(browser.getTitle()).to.equal('Google');
-    });
+describe('Search for Chimp BDD', function() {
+  before(function() {
+    browser.url('http://www.google.com');
+    browser.setValue('*[name="q"]', 'Chimp BDD')
+    browser.click('button[type="submit"]')
+  });
+  it('should reflect the current page title', function() {
+    expect(browser.waitForExist('a[href*="http://xolv.io"]', 3000))
   });
 });
